c7a2ed1076f1fad9e3126ab27595776601aeda52
LincolnITIT <kamphuislincoln@gmail.com>
2016-05-10 13:20:50 +0200

    Update csgodouble-bot-automated.js

--- csgodouble-bot-automated.js
+++ csgodouble-bot-automated.js
@@ -2,7 +2,7 @@
 // @name            csgodouble.com - automated
 // @description     An userscript that automates csgodouble.com betting using 6 chooseble systems.
 // @namespace       csgodouble.com - automated
-// @version         1.50
+// @version         1.40
 // @author          CrownMan
 // @match           http://www.csgodouble.com/
 // @match           http://www.csgodouble.com/index.php
